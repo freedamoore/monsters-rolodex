719b36e76259c8e696ab6b6c033df582809c31bd
Freeda Moore <freedamoore@outlook.com>
2020-04-01 14:36:53 +1100

    Updates

---
+++ precache-manifest.b2a1aae626f8b45b3ca7ea012ee8689a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c76229d21afd15aa90336fd5ef56971",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3de1966ba1b006b045d",
+    "url": "/monsters-rolodex/static/css/main.786a155b.chunk.css"
+  },
+  {
+    "revision": "9d36cb1448d0cd9bbf34",
+    "url": "/monsters-rolodex/static/js/2.9d52131b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.9d52131b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3de1966ba1b006b045d",
+    "url": "/monsters-rolodex/static/js/main.021fdbe4.chunk.js"
+  },
+  {
+    "revision": "ef9f4bf876b43359c128",
+    "url": "/monsters-rolodex/static/js/runtime-main.166aba63.js"
+  }
+]);
